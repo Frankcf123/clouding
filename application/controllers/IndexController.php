058f9b1c0755b1c53cce6027278cc81de23c3ca9
Frankcf329 <frankcf329@gmail.com>
2015-04-17 19:27:24 +1000

    fix bugs

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,9 +11,7 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
-                $this->render("header");
-                $this->render("index");
-
+            
     }
 
     public function examCodeCheckAction()

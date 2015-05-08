21b3a8da0338b42c6c47b2389d4c3ae6338a7b93
Frankcf329 <frankcf329@gmail.com>
2015-05-09 01:14:44 +1000

    update adding exam page

--- application/controllers/ExamController.php
+++ application/controllers/ExamController.php
@@ -1,35 +1,45 @@
 <?php
 
-class ExamController extends Zend_Controller_Action
-{
+class ExamController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         // action body
-        $this->view->page_name="Exam Info";
+        $this->view->page_name = "Exam Info";
     }
 
-    public function addExamAction()
-    {
-        // action body
-                $this->view->page_name="Exam Name";
-                
+    public function addExamAction($name) {
+
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $exam_name = $request->getParam("exam_name");
+            $exam_no = $request->getParam("exam_no");
+            $exam_duration = $request->getParam("exam_duration");
+            $this->view->exam_name = $exam_name;
+            $this->view->exam_no = $exam_no;
+            $this->view->exam_duration = $exam_duration;
+        }
+        $this->view->page_name = "Add exam page";
     }
 
-    public function addBasicInfoAction()
-    {
+    public function addBasicInfoAction() {
         // action body
+//        $this->redirect(root_url."/exam/add-exam?name=2");
+//        $this->addExamAction("chenfang");
     }
 
+    public function finisheditAction() {
+        // action body
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $exam_name = $request->getParam("exam_name");
+            $exam_no = $request->getParam("exam_no");
+            $exam_duration = $request->getParam("exam_duration");
+}
+    }
 
 }
-
-
-
-
-
---
+++ application/models/Exam.php
@@ -0,0 +1,8 @@
+<?php
+
+class Application_Model_Exam extends Zend_Db_Table_Abstract {
+
+    protected $_name = "user";
+    protected $_primary = "id";
+
+}
